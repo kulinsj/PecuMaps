f276ad2c305035c7a9f3bcaad659670e882c4ffc
Jan Kulinski <jan.t.kulinski@gmail.com>
2013-03-03 20:09:50 -0500

    quick commit to pull

--- MEDIUM/assets/www/js/MainScreen.js
+++ MEDIUM/assets/www/js/MainScreen.js
@@ -1,2 +1,23 @@
 $(document).ready(function(){
+	var map = new GoogleMap();
+    map.initialize();
 });
+
+function GoogleMap(){
+
+this.initialize = function(){
+var map = showMap();
+}
+
+ var showMap = function(){
+var mapOptions = {
+zoom: 12,
+center: new google.maps.LatLng(43.47,-80.54),
+mapTypeId: google.maps.MapTypeId.ROADMAP
+}
+
+var map = new google.maps.Map(document.getElementById("map_canvas"), mapOptions);
+
+return map;
+}
+}
