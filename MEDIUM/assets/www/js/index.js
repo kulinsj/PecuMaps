fa6173d67a7bad93824ac6f7b1020a0e3216a4e5
Tom Sun <tomsun100@gmail.com>
2013-03-10 21:18:44 -0400

    Log in and signup page and whatever

--- MEDIUM/assets/www/js/index.js
+++ MEDIUM/assets/www/js/index.js
@@ -1,4 +1,4 @@
-/*function hideLoadingPage(){
+function hideLoadingPage(){
     document.getElementById('loadingpage').style.display="none";
     document.getElementById('loginpage').style.display="block";
 }
@@ -6,25 +6,16 @@
 function delayedHideLoadingPage(){
     window.setTimeout(function() {
 	    hideLoadingPage();
-	}, 0);
+	}, 1700);
 }
 
-function activateLoginButton(){
-    document.getElementById("loginbutton").style.background='#02d238'
-	document.getElementById("signupbutton").style.background='-moz-linear-gradient(top, rgba(255,255,255,1) 0%,rgba(239,239,239,1) 60%,rgba(225,223,226,1) 100%);'
-	document.getElementById("confirmpassword").style.display='none'
-	}
 
-function activateSignupButton(){
-    document.getElementById("signupbutton").style.background='#02d238'
-	document.getElementById("loginbutton").style.background='-moz-linear-gradient(top, rgba(255,255,255,1) 0%,rgba(239,239,239,1) 60%,rgba(225,223,226,1) 100%);'
-	document.getElementById("confirmpassword").style.display='block'
-	}
-*/
 $().ready(function(){
 	var $loginbutton = $("#loginbutton");
 	var $signupbutton = $("#signupbutton");
 	var $confirmpassword = $("#confirmpassword");
+	var $goBtn = $("#goBtn");
+	var $goLink = $("#goLink");
 
 	$loginbutton.click(function(){
 		$loginbutton.addClass('selected');
@@ -37,4 +28,13 @@ $().ready(function(){
 		$loginbutton.removeClass('selected');
 		$confirmpassword.css("display", "block");
 	});
+
+	$goBtn.click(function(){
+		if ($loginbutton.hasClass('selected')){
+			$goLink.attr("href", "MainScreen.html");
+		}else{
+			$goLink.attr("href", "UserDetail.html");
+		}
+	});
+
 });
